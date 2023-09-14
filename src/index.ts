a715403c5ee20a4bd688bc1663f18d4d0fce5fdf
cr1stalcoma <cristalcomaa@gmail.com>
2023-09-14 21:43:49 +0300

    update commit

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 3003;
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello world...!")
+    res.send("Hello world!!!")
 });
 
 app.listen(port, () => {
